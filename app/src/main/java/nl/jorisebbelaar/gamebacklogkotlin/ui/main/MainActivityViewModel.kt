f87695eb313b59d9fee95125cbfbc8a394691f2b
frozenpeppers <joris-ebbelaar@hotmail.nl>
2020-12-14 08:44:11 +0100

    level5task2

---
+++ app/src/main/java/nl/jorisebbelaar/gamebacklogkotlin/ui/main/MainActivityViewModel.kt
@@ -0,0 +1,36 @@
+package nl.jorisebbelaar.gamebacklogkotlin.ui.main
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import nl.jorisebbelaar.gamebacklogkotlin.database.GameRepository
+import nl.jorisebbelaar.gamebacklogkotlin.model.Game
+
+class MainActivityViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val ioScope = CoroutineScope(Dispatchers.IO)
+    private val gameRepository = GameRepository(application.applicationContext)
+
+    val games: LiveData<List<Game>> = gameRepository.getAllGames()
+
+    fun insertGame(game: Game) {
+        ioScope.launch {
+            gameRepository.insertGame(game)
+        }
+    }
+
+    fun deleteAllGames() {
+        ioScope.launch {
+            gameRepository.deleteAllGames()
+        }
+    }
+
+    fun deleteGame(game: Game) {
+        ioScope.launch {
+            gameRepository.deleteGame(game)
+        }
+    }
+}
